b8fd26844c1da344b3f36fb6ed9500ab9ea8920e
Megan Rowan <megan.rowan.88@gmail.com>
2022-09-16 12:29:16 -0400

    commit tree 7bf6a9ad9920cd987341d254eb013153340ff92f

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Megan',
+    streetAddress: '12 Main',
+}
+
+function updateEmployeeWithKeyAndValue (employee, key, value){
+    const emp = {...employee};
+    emp[key] = value;
+
+    return emp;
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]= value;
+    return employee
+};
+function deleteFromEmployeeByKey(employee, key){
+    const emp1 = {...employee};
+    delete emp1[key]
+    return emp1
+};
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+    return employee
+}
